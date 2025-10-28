454cd15cbb1b4422d8e406d79dbd6078062dd649
Achraf <ibrahimachraf0250@gmail.com>
2025-10-28 18:33:43 +0100

    who's care

--- app/(auth)/sign-in/[[...rest]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs"
-
-export default function signinpage(){
-  return (
-    <div>
-      <SignIn withSignUp="false" signUpFallbackRedirectUrl="/dashboard"/>
-    </div>
-  )
-}
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>achraf</h1>
+      <h1>Home Page -_-</h1>
     </div>
   );
 }
